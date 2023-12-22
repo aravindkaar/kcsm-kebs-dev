045c95c43746b941f0037fafda3dacdf0b51170f
AravindKaar7 <aravindkaar@gmail.com>
2023-12-22 13:11:55 +0530

    fix

--- utils.py
+++ utils.py
@@ -45,7 +45,6 @@ def request_open_ai(extracted_text):
     
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo-16k",
-        response_format={'type':'json_object'},
         messages=[
             {"role": "system", "content": system_prompt},
             {"role": "user", "content": extracted_text}
